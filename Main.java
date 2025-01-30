98233304638849f0827684dd5c8268f5a7f9cba9
Caiden-C <118212089+Caiden-C@users.noreply.github.com>
2025-01-30 05:39:27 +0000

    added another print

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! from Caiden");
+        System.out.println("Hi, I'm a change!");
     }
 }
